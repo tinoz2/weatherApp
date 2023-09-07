b3838ff483f275e7c28c170edcf8afb735b38a4c
Santino <mellamoalroberto@gmail.com>
2023-09-07 20:27:16 -0300

    changeCSS

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const apiKey = "f8988a03465a9a50037d22248499afb9"
+const apiKey = "49571687011636cae3410c860d9560df"
 const fetchData = position => {
     const {latitude, longitude} = position.coords;
     fetch(`https://api.openweathermap.org/data/3.0/onecall?units=metric&lat=${latitude}&lon=${longitude}&appid=${apiKey}`)
